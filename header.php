814f06931f482a1d26eb4e92de5448b12e093756
Kaito0712 <148739905+Kaito0712@users.noreply.github.com>
2024-01-16 13:38:03 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP Sample Programs</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
+</head>
+<body>
